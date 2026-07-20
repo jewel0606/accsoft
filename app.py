c22bc4b87d1f414d95467f009e3a4a924d95d7e4
yaqin jewel <yaqin.jewel00@gmail.com>
2026-07-20 21:01:22 +0600

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def show_report(view_name):
             st.dataframe(
                 df,
                 use_container_width=True,
-                height=6000
+                height=2000
             )
         else:
             st.info("No data found.")
